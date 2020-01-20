ec261dcdccfba7d2677efaa21d3959573b4f96e2
user <gngh0101@gmail.com>
2020-01-21 01:08:49 +0900

    init

---
+++ src/test/java/com/github/hotire/spring/rsocket/SpringRsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotire.spring.rsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRsocketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
