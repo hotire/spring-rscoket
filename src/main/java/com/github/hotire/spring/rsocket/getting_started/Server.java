ef198e914648ab15aa8e81ad7b8cb0c4b813edbb
hotire <gngh0101@gmail.com>
2020-02-11 00:40:14 +0900

    Implement fireAndForget

--- src/main/java/com/github/hotire/spring/rsocket/getting_started/Server.java
+++ src/main/java/com/github/hotire/spring/rsocket/getting_started/Server.java
@@ -4,11 +4,20 @@ import io.rsocket.AbstractRSocket;
 import io.rsocket.Payload;
 import io.rsocket.RSocketFactory;
 import io.rsocket.transport.netty.server.TcpServerTransport;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class Server {
+
     private final Disposable server;
+    @Getter
+    private final List<Payload> data = new ArrayList<>();
 
     public Server(final int port) {
         this.server = RSocketFactory.receive()
@@ -26,6 +35,16 @@ public class Server {
         @Override
         public Mono<Payload> requestResponse(Payload payload) {
             return Mono.just(payload);
+        }
+        @Override
+        public Mono<Void> fireAndForget(Payload payload) {
+            try {
+                log.info("payload : {}", payload);
+                data.add(payload);
+                return Mono.empty();
+            } catch (Exception x) {
+                return Mono.error(x);
+            }
         }
     }
 }
