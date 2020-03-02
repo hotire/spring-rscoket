1adca6fc6a62d3a69afcb4569a2cc622433a36d5
hotire <gngh0101@gmail.com>
2020-03-03 01:19:11 +0900

    Add ChannelClient

---
+++ src/main/java/com/github/hotire/spring/rsocket/getting_started/channel/ChannelClient.java
@@ -0,0 +1,36 @@
+package com.github.hotire.spring.rsocket.getting_started.channel;
+
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChannelClient {
+
+    private final RSocket socket;
+    @Getter
+    private final RSocketClientController clientController;
+
+    public ChannelClient(final int port) {
+        this.socket = RSocketFactory.connect()
+                                    .transport(TcpClientTransport.create("localhost", port))
+                                    .start()
+                                    .block();
+        this.clientController = new RSocketClientController(new ArrayList<>());
+    }
+
+    public void process(final List<Payload> messages) {
+        clientController.addMessages(messages);
+        socket.requestChannel(clientController)
+              .doOnNext(clientController::process)
+              .blockLast();
+    }
+
+    public void dispose() {
+        this.socket.dispose();
+    }
+}
