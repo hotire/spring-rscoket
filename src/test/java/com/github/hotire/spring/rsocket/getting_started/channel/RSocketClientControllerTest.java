c07da30ca504133fefbb835565120f04b9c6071b
hotire <gngh0101@gmail.com>
2020-03-18 00:30:11 +0900

    Verifty addMessages

--- src/test/java/com/github/hotire/spring/rsocket/getting_started/channel/RSocketClientControllerTest.java
+++ src/test/java/com/github/hotire/spring/rsocket/getting_started/channel/RSocketClientControllerTest.java
@@ -1,5 +1,26 @@
 package com.github.hotire.spring.rsocket.getting_started.channel;
 
-class RSocketClientControllerTest {
+import io.rsocket.Payload;
+import io.rsocket.util.DefaultPayload;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class RSocketClientControllerTest {
+    @Test
+    void addMessages() {
+        // given
+        final List<Payload> payloads = mock(List.class);
+        final RSocketClientController clientController = new RSocketClientController(payloads);
+
+        // when
+        clientController.addMessages(Lists.newArrayList(DefaultPayload.create("hello")));
+
+        // then
+        verify(payloads, times(1)).addAll(any());
+    }
 }
