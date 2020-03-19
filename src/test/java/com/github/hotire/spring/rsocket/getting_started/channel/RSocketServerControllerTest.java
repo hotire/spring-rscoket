a76796522112fdb8df7c54f13976e991c476b85b
hotire <gngh0101@gmail.com>
2020-03-20 01:17:32 +0900

    Verify subscribe in rSocketServerController

--- src/test/java/com/github/hotire/spring/rsocket/getting_started/channel/RSocketServerControllerTest.java
+++ src/test/java/com/github/hotire/spring/rsocket/getting_started/channel/RSocketServerControllerTest.java
@@ -1,10 +1,13 @@
 package com.github.hotire.spring.rsocket.getting_started.channel;
 
+import io.rsocket.Payload;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class RSocketServerControllerTest {
 
@@ -13,5 +16,19 @@ class RSocketServerControllerTest {
         // when then
         assertThat(new RSocketServerController("", 0)).isNotNull();
         assertThat(new RSocketServerController("", 0, Map.of())).isNotNull();
+    }
+
+    @Test
+    void subscribe() {
+        // given
+        final Subscriber<? super Payload> subscriber = mock(Subscriber.class);
+        final RSocketServerController rSocketServerController = new RSocketServerController("", 0);
+
+        // when
+        rSocketServerController.subscribe(subscriber);
+
+        // then
+        verify(subscriber, times(1)).onNext(any());
+        verify(subscriber, times(1)).onComplete();
     }
 }
