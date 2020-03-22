e4142331bbdee6f6a2ef1a68c30710432311bd81
hotire <gngh0101@gmail.com>
2020-03-23 00:41:06 +0900

    Verify getClientController

--- src/test/java/com/github/hotire/spring/rsocket/getting_started/channel/ChannelClientTest.java
+++ src/test/java/com/github/hotire/spring/rsocket/getting_started/channel/ChannelClientTest.java
@@ -4,13 +4,16 @@ import com.github.hotire.spring.rsocket.getting_started.Server;
 import io.rsocket.util.DefaultPayload;
 import me.alexpanov.net.FreePortFinder;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ChannelClientTest {
 
     @Test
-    void process_() {
+    void process() {
         // given
         final int port = FreePortFinder.findFreeLocalPort();
         final Server server = new Server(port);
@@ -20,6 +23,22 @@ class ChannelClientTest {
         channelClient.process(List.of(DefaultPayload.create("hello")));
 
         // then
+        channelClient.dispose();
+        server.dispose();
+    }
+
+    @Test
+    void getClientController() {
+        // given
+        final int port = FreePortFinder.findFreeLocalPort();
+        final Server server = new Server(port);
+        final ChannelClient channelClient = new ChannelClient(port);
+
+        // when
+        final RSocketClientController result = channelClient.getClientController();
+
+        // then
+        assertThat(result).isInstanceOf(Publisher.class);
         channelClient.dispose();
         server.dispose();
     }
