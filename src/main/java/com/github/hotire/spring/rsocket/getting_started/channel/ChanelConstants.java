614921cdb2a436dc57d095591e0958033979882f
hotire <gngh0101@gmail.com>
2020-03-23 00:30:56 +0900

    Add ChanelConstants

---
+++ src/main/java/com/github/hotire/spring/rsocket/getting_started/channel/ChanelConstants.java
@@ -0,0 +1,10 @@
+package com.github.hotire.spring.rsocket.getting_started.channel;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChanelConstants {
+    public static final String EXIT = "exit";
+    public static final String LOCAL = "localhost";
+}
